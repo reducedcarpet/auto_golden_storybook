91d60d9f74e7f6979ae9db3b5464274a5952f282
reducedcarpet <michaelcadenpike@gmail.com>
2024-02-09 15:14:16 +0000

    Some cleaning up.

--- lib/code_gen/code_gen_constants.dart
+++ lib/code_gen/code_gen_constants.dart
@@ -1,5 +1,2 @@
+// file name for file containing the list of Story objects.
 const storiesFileName = 'stories.dart';
-const widgetStoriesFileName = 'widget_stories.dart';
-const goldenStoriesFileName = 'golden_stories.dart';
-const scaffoldStoriesFileName = 'scaffold_stories.dart';
-const routingStoriesFileName = 'routing_stories.dart';
